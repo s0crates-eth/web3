13f535f89d24c36585ac251c981dc9b3fe8bc3f1
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-01-01 20:50:37 +0700

    Rename .eslintrc copy.js to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,24 @@
 module.exports = {
-  parserOptions: {
-    'sourceType': 'script',
-    'ecmaVersion': 2017,
-    'ecmaFeatures': {
-      'experimentalObjectRestSpread': true,
-      'impliedStrict': true,
-      'modules': true,
-      'blockBindings': true,
-      'arrowFunctions': true,
-      'objectLiteralShorthandMethods': true,
-      'objectLiteralShorthandProperties': true,
-      'templateStrings': true,
-      'classes': true,
-    },
-  },
+  root: true,
 
   env: {
-    'browser': true,
+    browser: true,
   },
-
-  plugins: [
-    'json',
-  ],
-
-  extends: [
-    '@metamask/eslint-config',
-    '@metamask/eslint-config/config/nodejs',
-  ],
 
   globals: {
-    'web3': true,
+    ethereum: 'readonly',
   },
-}
+
+  extends: ['@metamask/eslint-config', '@metamask/eslint-config-nodejs'],
+
+  overrides: [
+    {
+      files: ['src/*.js'],
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+  ],
+
+  ignorePatterns: ['!.eslintrc.js', 'dist'],
+};
