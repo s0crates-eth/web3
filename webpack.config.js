a45d16f57f197801e4fbf18ea38a2bf7d8cd87d3
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-01-03 15:38:21 +0700

    Restart
    - completely wiped and put a fresh OFFICIAL in its place
    - have included the old HTML etc files
    - need to clean up the OFFICIAL files and keep it working
    - then merge in MY stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,37 @@
-const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
-const DIST = path.resolve(__dirname, 'dist')
+const DIST = path.resolve(__dirname, 'dist');
 
 module.exports = {
-  parserOptions: {
-    'sourceType': 'script',
-    'ecmaVersion': 2017,
-    'ecmaFeatures': {
-      'experimentalObjectRestSpread': true,
-      'impliedStrict': true,
-      'modules': true,
-      'blockBindings': true,
-      'arrowFunctions': true,
-      'objectLiteralShorthandMethods': true,
-      'objectLiteralShorthandProperties': true,
-      'templateStrings': true,
-      'classes': true,
-    },
+  devtool: 'eval-source-map',
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: DIST,
+    publicPath: DIST,
   },
-
-  globals: {
-    'web3': 'readonly',
-    'ethereum': 'readonly',
+  devServer: {
+    contentBase: DIST,
+    port: 9011,
+    writeToDisk: true,
   },
-
-  env: {
-    'browser': true,
-  },
-
   plugins: [
-    'json',
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+
+    // for build scripts
+    new CopyPlugin({
+      patterns: [
+        {
+          flatten: true,
+          from: './src/*',
+          globOptions: {
+            ignore: ['**/*.js'],
+          },
+        },
+      ],
+    }),
   ],
-
-  extends: [
-    '@metamask/eslint-config',
-    '@metamask/eslint-config/config/nodejs',
-  ],
-
-  overrides: [{
-    'files': ['src/index.js'],
-    'parserOptions': {
-      'sourceType': 'module',
-    },
-  }],
-
-  globals: {
-    'web3': true,
-    'ethereum': true,
-  },
-}
-
+};
